2dd7caaac0ae52cdf5c38b7c3cbab549d9c54586
lishenghua <icepoly@gmail.com>
2018-04-09 14:41:11 +0800

    update sql

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -16,9 +16,9 @@ module.exports = async (ctx, next) => {
     // 通过 Koa 中间件进行登录态校验之后
     // 登录信息会被存储到 ctx.state.$wxInfo
     // 具体查看：
-    if (ctx.state.$wxInfo.loginState === 1) {
+    if (ctx.state.$wxInfo.loginState === 1 && ctx.state.$wxInfo.userinfo) {
         // loginState 为 1，登录态校验成功
-        DB('cUserInfo').whereRaw('open_id = ?',open_id).then(res =>{
+        DB('cUserInfo').whereRaw('open_id = ?',ctx.state.$wxInfo.userinfo.openId).then(res =>{
             if(JSON.stringify(res) == "[]"){
                 ctx.state.code = -1
             }
