ece7f28f8066ed7862edac46fa6f2edb2dd75834
lishenghua <icepoly@gmail.com>
2018-04-09 17:42:06 +0800

    update sql

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,16 @@
+const db = require('../middlewares/util')
+
 module.exports = async (ctx, next) => {
     // 通过 Koa 中间件进行登录态校验之后
     // 登录信息会被存储到 ctx.state.$wxInfo
     // 具体查看：
     if (ctx.state.$wxInfo.loginState === 1) {
         // loginState 为 1，登录态校验成功
-        ctx.state.data = ctx.state.$wxInfo.userinfo
+        var check = db.checkUserInfo(ctx.state.$wxInfo.userinfo.openId)
+        await check.then(res =>{
+            if(res != -1){
+                ctx.state.data = ctx.state.$wxInfo.userinfo
+            }})
     } else {
         ctx.state.code = -1
     }
