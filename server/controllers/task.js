0066bcb10e38badb17bc577092db1fd90c92417a
lishenghua <icepoly@gmail.com>
2018-04-09 14:39:38 +0800

    update sql

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -12,29 +12,23 @@ const DB = require('knex')({
     }
 })
 
-var checkUserInfo = (open_id) => {
-    DB('cUserInfo').whereRaw('open_id = ?',open_id).then(res =>{
-        if(JSON.stringify(res) == "[]"){
-            return false
-        }
-        else {
-            return true
-        }}, err => {
-            return false
-        })
-}
-
 module.exports = async (ctx, next) => {
     // 通过 Koa 中间件进行登录态校验之后
     // 登录信息会被存储到 ctx.state.$wxInfo
     // 具体查看：
-    if (ctx.state.$wxInfo.loginState === 1 && checkUserInfo(ctx.state.$wxInfo.open_id)) {
+    if (ctx.state.$wxInfo.loginState === 1) {
         // loginState 为 1，登录态校验成功
-        checkUserInfo(ctx.state.$wxInfo.open_id).then(res =>{
-            const body = ctx.request.body
-            ctx.state.code = 0
-            ctx.body = 'success'
-        })
+        DB('cUserInfo').whereRaw('open_id = ?',open_id).then(res =>{
+            if(JSON.stringify(res) == "[]"){
+                ctx.state.code = -1
+            }
+            else {
+                const body = ctx.request.body
+                ctx.state.code = 0
+                ctx.body = 'success'
+            }}, err => {
+                ctx.state.code = -1
+            })
     } else {
         ctx.state.code = -1
     }
