8e4933e94c0d0410b4210262508c99e718740639
lishenghua <icepoly@gmail.com>
2018-04-10 17:43:08 +0800

    update server

--- server/middlewares/db.js
+++ server/middlewares/db.js
@@ -24,6 +24,23 @@ async function checkUserInfo(open_id){
         })
 }
 
+async function updateTaskInfo(open_id){
+    return DB('cTaskInfo').where('open_id', '=', open_id).where('state', '=', 0).update({
+        state: 1,
+        type: data.type,
+        optype: data.optype,
+        opdata: data.opdata,
+      }).then(res =>{
+          if(res === 0){
+              return 0x10
+          }
+          else {
+              return 0
+          }}, err => {
+              return 0x11
+          })
+}
+
 async function addTask(open_id, data){
     var check = checkUserInfo(open_id)
     return await check.then(res => {
@@ -32,22 +49,6 @@ async function addTask(open_id, data){
             }
             else
             {
-                async function updateTaskInfo(open_id){
-                    return DB('cTaskInfo').where('open_id', '=', open_id).where('state', '=', 0).update({
-                        state: 1,
-                        type: data.type,
-                        optype: data.optype,
-                        opdata: data.opdata,
-                      }).then(res =>{
-                          if(res === 0){
-                              return 0x10
-                          }
-                          else {
-                              return 0
-                          }}, err => {
-                              return 0x11
-                          })
-                }
                 var update = updateTaskInfo(open_id)
                 return await update.then(res => {
                     return res
@@ -56,55 +57,58 @@ async function addTask(open_id, data){
 
 }
 
-async function acceptJob(open_id){
-    var check = checkUserInfo(open_id)
+async function queryJobInfo(open_id){
     var data = {}
+    DB('cTaskInfo').where('state', '=', 1).whereRaw('open_id = ?',open_id).then(res =>{
+        if(JSON.stringify(res) == "[]"){
+            return data
+        }
+        else {
+            data.type = res[0].type
+            data.optype =res[0].optype
+            data.opdata =res[0].opdata        
+            return data
+        }}, err => {
+            return data
+        })    
+}
+
+async function updateJobInfo(open_id){
+    return DB('cTaskInfo').where('open_id', '=', open_id).where('state', '=', 1).update({
+        state: 0
+      }).then(res =>{
+          if(res === 0){
+              return false
+          }
+          else {
+              return true
+  }}, err => {
+      return false
+  })
+}
+
+async function acceptJob(open_id){
+    var data = {}
+    var check = checkUserInfo(open_id)
     return await check.then(res => {
             if (res <= 0) {
                 return data
             }
-            else
-            {
-                async function queryJobInfo(open_id){
-                    DB('cTaskInfo').where('state', '=', 1).whereRaw('open_id = ?',open_id).then(res =>{
-                        if(JSON.stringify(res) == "[]"){
-                            return data
-                        }
-                        else {
-                            data.type = res[0].type
-                            data.optype =res[0].optype
-                            data.opdata =res[0].opdata        
-                            return data
-                        }}, err => {
-                            return data
-                        })
-
-                async function updateJobInfo(open_id){
-                    return DB('cTaskInfo').where('open_id', '=', open_id).where('state', '=', 1).update({
-                        state: 0
-                      }).then(res =>{
-                          if(res === 0){
-                              return false
-                          }
-                          else {
-                              return true
-                  }}, err => {
-                      return false
-                  })
-
-                  var query = queryJobInfo(open_id)
-                  return await query.then(res => {
-
+            else {
+                var query = queryJobInfo(open_id)
+                return await query.then(res => {
                     var update = updateJobInfo(open_id)
                     return await update.then(ret => {
                         if(ret){
                             return res
                         }
-                        else{
+                        else {
                             return data;
                         }
                     })
                 })
-
+            }
+        })
 }
+
 module.exports = { AddTask, AcceptJob }
