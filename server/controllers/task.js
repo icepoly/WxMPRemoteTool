776f48f9b5d78b9380a083630c253d003848ceb1
lishenghua <icepoly@gmail.com>
2018-04-10 12:01:38 +0800

    update sql

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -8,16 +8,15 @@ module.exports = async (ctx, next) => {
         // loginState 为 1，登录态校验成功
         var check = db.checkUserInfo(ctx.state.$wxInfo.userinfo.openId)
         await check.then(res => {
+            if (res <= 0) {
+                ctx.state.code = -1
+            }
+        })
+
+        var update = db.updateTaskState(ctx.state.$wxInfo.userinfo.openId, ctx.request.body, 1)
+        await update.then(res => {
             if (res > 0) {
-                var update = db.updateTaskState(ctx.state.$wxInfo.userinfo.openId, ctx.request.body, 1)
-                await update.then(res => {
-                    if (res > 0) {
-                        ctx.body = 'success'
-                    }
-                    else{
-                        ctx.state.code = -1
-                    }
-                })
+                ctx.body = 'success'
             }
             else{
                 ctx.state.code = -1
